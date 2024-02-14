f83f9322b18f83791c37296486bfd015a209724a
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-14 22:34:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ class Employee extends Person{
 		
 	}
 	jobGreet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}. `)
+		    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 	}
 }
 const alice = new Person("Alice",25);
@@ -37,5 +37,5 @@ const bob = new Employee("Bob","30","Manager");
 bob.greet();
 bob.jobGreet();
 // Do not change code below this line
-// window.Person = Person;
-// window.Employee = Employee;
+window.Person = Person;
+window.Employee = Employee;
